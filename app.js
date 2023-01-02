b115909984f82c358a3847decb746fb38462275f
Kim gimin <ggm4954@gmail.com>
2023-01-02 14:03:39 +0900

    comment

--- app.js
+++ app.js
@@ -49,7 +49,7 @@
 
 
 
-
+// // Session
 // const express = require('express')
 // const cookieParser = require('cookie-parser')
 
@@ -81,7 +81,7 @@
 //     const ssid = Date.now().toString()
 
 //     sessions.push({
-//         ...user,
+//         ...user, 
 //         ssid
 //     })
 
@@ -101,7 +101,7 @@
 // })
 
 
-
+// express-session
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
@@ -114,35 +114,51 @@ app.use(cookieParser())
 app.use(express.json())
 
 const sessionObj = {
-    secret: 'aaa',
+    // secret(필수옵션) : session key, 세션 및 쿠키 확인
+    secret: 'kong',
+    // resave : 세션값 변동 여부와 상관없이 다시 저장할지
     resave: false,
+    // saveUninitialized : session에 내용 여부와 상과없이 저장할지
     saveUninitialized: true,
+    // store : 세션을 저장하는 곳/ memorystorer 모듈을 이용한 새로운 저장소
     store: new MemoryStore(),
 }
 
-app.use(session(sessionObj))
+app.use(session(sessionObj)) // session 
 
 const users = [
-    {id: "noggong"},
-    {id: "hyowon"},
-    {id: "kimin"}
+    { id: "noggong" },
+    { id: "hyowon" },
+    { id: "kimin" }
 ]
 
-app.get('/users', (req,res) => {
-    const userId = req.session.userId
+// 유저 정보 확인
+app.get('/users', (req, res) => {
+    //express-session으로 req.session 사용 가능
+    const userId = req.session.userId // sessntion.usersId를 userId에 할당
+
     const user = users.find(user => user.id === userId)
+
+    if (!user) {
+        return res.status(401).json({ message: "로그인 이후 사용" })
+    }
+    res.send(user)
+
+
     res.send(user)
 })
 
 app.post('/login', (req, res) => {
+    //body로 userId 보내줌
     const userId = req.body.userId
     const user = users.find(user => user.id === userId)
 
-    req.session.userid = user.id
+    req.session.userid = user.id // userid값을 session의 userid값에 할당
+    // console.log(req.session) //세션을 확인하기 위한 콘솔
     res.send(user.id)
 })
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req, res) => {
     res.send("logout page")
 })
 
